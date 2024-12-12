a0d377b60699f6837d9a47b814b554351655cf0a
Francisca <francisca.paredes.pr@gmail.com>
2024-12-12 20:15:21 -0300

    list.html

--- educa/urls.py
+++ educa/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from courses.views import CourseListView
 
 urlpatterns = [
     path(
@@ -29,7 +30,12 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
+
+
+
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
