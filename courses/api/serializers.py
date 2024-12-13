4773e21f0727f892673c32eb6fb92f611ba15116
Francisca <francisca.paredes.pr@gmail.com>
2024-12-13 14:40:50 -0300

    total_courses SubjectSerializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -4,7 +4,8 @@ from courses.models import Subject
 
 # Serializador para el modelo de subject
 class SubjectSerializer(serializers.ModelSerializer):
-    
+    total_courses = serializers.IntegerField()
+
     class Meta:
         model = Subject    # Especificamos el modelo a serializar
-        fields = ['id', 'title', 'slug']    # Especificamos los campos del modelos en el fields
+        fields = ['id', 'title', 'slug', 'total_courses']    # Especificamos los campos del modelos en el fields
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
 from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
+from django.db.models import Count
 
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()    # Conjunto de consultas base para recuperar objectos
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))     # Conjunto de consultas base para recuperar objectos - anotamos el conteo de cursos relacionados
     serializer_class = SubjectSerializer    # La clase para serializar objetos
 
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
