b0fdbead60409438ffc1b12ce1e359023b2acad9
Francisca <francisca.paredes.pr@gmail.com>
2024-12-16 10:53:21 -0300

    room.html addevent

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,12 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 # Consumidor
 class ChatConsumer(WebsocketConsumer):
     def connect(self):    # Mètodo connect(), se llama cuandos e recibe una nueva conexiòn
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']    # Recuperamos el parametro coruse_id de la url. cada consumidor tiene un scope con infromaciòn sobre su conexiòn.
         self.room_group_name = f'chat_{self.id}'
         # join room group
@@ -26,12 +28,16 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):    # Se llama cada vez que se recibe un datos desde el WebSocket. Se espera que los datos sean texto recibido como text_data. Tratamos los datos de texto recibidos como JSON
         text_data_json = json.loads(text_data)
         message = text_data_json['message']    # Utilizamos json.loads() para cargar los datos JSON recibidos en un diccionario de Python.
+        now = timezone.now()
+
         # send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',    # EL tipo de evento, esta es una clave especial que corresponde al nombre del mètodo que debe ser invocado en los consumidores que reciben el evento.
                 'message': message,    # Mensaje real que se esta enviando.
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
